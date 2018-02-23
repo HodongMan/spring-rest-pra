a9282cd1e2044c12e0bed3fd61443db2e77b0860
hodongman <arshavin3@naver.com>
2018-02-23 19:03:55 +0900

    hateoas test

--- src/main/java/com/apress/spring/SpringBootHodongApplication.java
+++ src/main/java/com/apress/spring/SpringBootHodongApplication.java
@@ -2,8 +2,18 @@ package com.apress.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @SpringBootApplication
 public class SpringBootHodongApplication {
@@ -13,7 +23,7 @@ public class SpringBootHodongApplication {
 	}
 }
 
-class Greet {
+class Greet extends ResourceSupport{
 	
 	private String message;
 	public Greet() {}
@@ -28,11 +38,27 @@ class Greet {
 	public void setMessage(String message) {
 		this.message = message;
 	}
+
+	public String toString() {
+		return message;
+	}
 }
 
 @RestController
 class GreetingController {
 	
+	@RequestMapping("/greeting")
+	@ResponseBody
+	public HttpEntity<Greet> greeting(@RequestParam(value = "name", required = false, defaultValue = "HATEOAS") String name) {
+		Greet greet = new Greet("Hello " + name);
+		greet.add(linkTo(
+			methodOn(GreetingController.class)
+			.greeting(name))
+			.withSelfRel()
+		);
+		return new ResponseEntity<Greet>(greet, HttpStatus.OK);
+	}
+
 	@GetMapping("/")
 	Greet greet() {
 		return new Greet("Hello World!");
